8c28a8a06c721ed170aaff69ee7520efea5346c4
Ankit Patel <patelankit15123@gmail.com>
2025-10-11 14:18:59 +0530

    Fix backend signup/signin issue

--- backand/server.js
+++ backand/server.js
@@ -52,7 +52,8 @@ app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    origin: '*',
+    credentials: true,
 }));
 
 
